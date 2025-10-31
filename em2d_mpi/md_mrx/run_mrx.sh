84d83e9d19e2b5b4813b72b1383cb609f5a9cad3
shok <shohgo.okazaki2@gmail.com>
2025-10-31 16:56:40 +0900

    debug

--- em2d_mpi/md_mrx/run_mrx.sh
+++ em2d_mpi/md_mrx/run_mrx.sh
@@ -2,8 +2,16 @@
 
 #PBS -q A_S
 #PBS -P NIFS25KIST067
-#PBS -l select=1:ncpus=12:mem=64gb
+#PBS -l select=1:ncpus=12:mpiprocs=12:ompthreads=1:mem=64gb
 #PBS -l walltime=03:00:00
 module load intel/2025.1
-cd /home/shok/pcans/em2d_mpi/md_mrx
-./a.out
+module load openmpi/5.0.7/gcc11.5.0
+
+cd ${PBS_O_WORKDIR}
+
+export FC=$(which mpif90)
+
+make clean
+make
+
+mpirun ./a.out
