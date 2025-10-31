0556a529b28b014157e675c02424f5da88344f91
shok <shohgo.okazaki2@gmail.com>
2025-10-31 15:07:45 +0900

    test run

--- em2d_mpi/md_mrx/run_mrx.sh
+++ em2d_mpi/md_mrx/run_mrx.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#PBS -q A_dev
+#PBS -q A_S
 #PBS -P NIFS25KIST067
-#PBS -l select=1:ncpus=12:mem=6gb
-#PBS -l walltime=00:5:00
+#PBS -l select=1:ncpus=12:mem=64gb
+#PBS -l walltime=03:00:00
 module load intel/2025.1
 ./a.out
