f059630bbb4336ea0a61000b9c182a9cd9a397f6
shok <shohgo.okazaki2@gmail.com>
2025-10-31 16:30:47 +0900

    debug

--- em2d_mpi/md_mrx/run_mrx_initial.sh
+++ em2d_mpi/md_mrx/run_mrx_initial.sh
@@ -2,8 +2,8 @@
 
 #PBS -q A_dev
 #PBS -P NIFS25KIST067
-#PBS -l select=1:ncpus=12:mpiprocs=12:mem=6gb
+#PBS -l select=1:ncpus=12:mpiprocs=12:ompthreads=1:mem=6gb
 #PBS -l walltime=00:5:00
 module load intel/2025.1
 cd /home/shok/pcans/em2d_mpi/md_mrx
-./a.out
+mpirun ./a.out
