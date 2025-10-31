cfa9ea06b11a3eb82d29db20e68acc2b08cabfe0
shok <shohgo.okazaki2@gmail.com>
2025-10-31 16:45:41 +0900

    debug

--- test/helloworld.sh
+++ test/helloworld.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q A_dev
 #PBS -P NIFS25KIST067
-#PBS -l select=1:ncpus=1:mem=6gb
+#PBS -l select=1:ncpus=4:mpiprocs=4:ompthreads=1:mem=6gb
 #PBS -l walltime=00:5:00
 
 module load intel/2025.1
